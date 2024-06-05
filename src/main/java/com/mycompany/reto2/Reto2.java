db382bf3be8767c23ceab070f61191f1e25ad3e3
gmd02 <gmd02@DESKTOP-64D7HTD>
2024-06-05 22:48:09 +0200

    1st Commit

---
+++ src/main/java/com/mycompany/reto2/Reto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto2;
+
+/**
+ *
+ * @author gmd02
+ */
+public class Reto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
